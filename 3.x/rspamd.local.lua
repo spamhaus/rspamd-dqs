66ec2a8ad000bba49199619cd2341573faf0f465
Patrick McIntyre <patrickm@spamhaus.org>
2024-02-20 10:52:45 +0100

    use regexp with re_condition to find listed cryptowallets, to allow use of hyperscan.

--- 3.x/rspamd.local.lua
+++ 3.x/rspamd.local.lua
@@ -15,120 +15,92 @@ local slow_stat = 10.0
 local fast_stat = 1.0
 local download_check = 2 * 24 * 3600.0
 
-local function check_cw_callback ( task, re, lowercase, cryptovalue )
-    local parts = task:get_text_parts()
-    if not parts then return false end
-    local r = task:get_resolver()
-    for _, part in ipairs(parts) do
-        local words = part:get_words('raw')
-        for _, word in ipairs(words) do
-        if (lowercase == 1) then word = string.lower(word) end
-            local match = re:match(word)
-            if match then
-                local hash = rspamd_hash.create_specific('sha1', word):hex()
-                rspamd_logger.infox('HASH ' .. hash)
+local function sh_register_cw_symbol(cwinfo)
+    local name = "RBL_SPAMHAUS_CW_" .. cwinfo.cryptovalue
+    config.regexp[name] = {
+        description = cwinfo.description,
+        group = cwinfo.group,
+        score = cwinfo.score,
+        re = cwinfo.re,
+        re_conditions = {
+            [cwinfo.re] = function (task, txt, s, e)
+                if dqs_key == nil then
+                    return false
+                end
+                local crypto = txt:sub(s + 1, e)
+                if cwinfo.lowercase then
+                    crypto = crypto:lower()
+                end
+                local hash = rspamd_hash.create_specific('sha256', crypto):base32('rfc')
                 local lookup = hash .. '._cw.' .. dqs_key .. hbl_spamhaus
-                local function dns_cb(_,_,results,err)
-                    if (not results) then return false end
-                    if (string.find(tostring(results[1]), '127.0.')) then
-                        rspamd_logger.infox('found ' .. cryptovalue .. ' wallet %s (hashed: %s) in Cryptowallet blocklist', word, hash)
-                        return task:insert_result('RBL_SPAMHAUS_CW_' .. cryptovalue, 1.0, word);
+                local function dns_cb(_, _, results, err)
+                    if not results or err ~= nil then
+                        return false
+                    end
+                    if string.find(tostring(results[1]), '127.0.') then
+                        rspamd_logger.infox('found %s wallet %s (hashed: %s) in Crypto blocklist', cwinfo.cryptovalue, crypto, hash)
+                        return task:insert_result(name, 1.0, crypto)
                     end
                 end
-                r:resolve_a({ task = task, name = lookup , callback = dns_cb, forced = true })
+                task:get_resolver():resolve_a({ task = task, name = lookup, callback = dns_cb, forced = true})
+                return false
             end
-        end
-    end
+        }
+    }
 end
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_BTC",
+sh_register_cw_symbol({
+    cryptovalue = "BTC",
     score = 7.0,
     description = "BTC found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- BTC regex
-        local re = rspamd_re.create_cached('^(?:bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$')
-        local lowercase = 0
-        local cryptovalue = "BTC"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\b(?:bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}\b/AL{sa_body}]],
+    lowercase = false,
 })
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_ETH",
+sh_register_cw_symbol({
+    cryptovalue = "ETH",
     score = 7.0,
     description = "ETH found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- ETH regex
-        local re = rspamd_re.create_cached('^0x[a-fA-F0-9]{40}$')
-        local lowercase = 1
-        local cryptovalue = "ETH"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\b0x[a-fA-F0-9]{40}\b/AL{sa_body}]],
+    lowercase = true,
 })
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_BCH",
+sh_register_cw_symbol({
+    cryptovalue = "BCH",
     score = 7.0,
     description = "BCH found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- BCH regex
-        local re = rspamd_re.create_cached('(?<!=)bitcoincash:(?:q|p)[a-z0-9]{41}')
-        local lowercase = 0
-        local cryptovalue = "BCH"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\bbitcoincash:(?:q|p)[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{41,111}\b/AL{sa_body}]],
+    lowercase = false,
 })
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_XMR",
+sh_register_cw_symbol({
+    cryptovalue = "XMR",
     score = 7.0,
     description = "XMR found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- XMR regex
-        local re = rspamd_re.create_cached('^(?:4(?:[0-9]|[A-B])(?:.){93})$')
-        local lowercase = 0
-        local cryptovalue = "XMR"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\b[4578][1-9A-HJ-NP-Za-km-z]{94}\b/AL{sa_body}]],
+    lowercase = false,
 })
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_LTC",
+sh_register_cw_symbol({
+    cryptovalue = "LTC",
     score = 7.0,
     description = "LTC found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- LTC regex
-        local re = rspamd_re.create_cached('^(?:[LM3][a-km-zA-HJ-NP-Z1-9]{26,33})$')
-        local lowercase = 0
-        local cryptovalue = "LTC"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\b[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}\b/AL{sa_body}]],
+    lowercase = false,
 })
 
-rspamd_config:register_symbol({
-    name = "RBL_SPAMHAUS_CW_XRP",
+sh_register_cw_symbol({
+    cryptovalue = "XRP",
     score = 7.0,
     description = "XRP found in Spamhaus cryptowallet list",
     group = "spamhaus",
-    type = "callback",
-    callback = function(task)
-        -- XRP regex
-        local re = rspamd_re.create_cached('^(?:r[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35})$')
-        local lowercase = 0
-        local cryptovalue = "XRP"
-        check_cw_callback (task, re, lowercase, cryptovalue )
-    end
+    re = [[/\br[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35}\b/AL{sa_body}]],
+    lowercase = false,
 })
 
 local function process_alg(urltable, alg)
